26d54488b61dbc80d14cb7670278cd527e91e97c
Daniel Ferguson <dan.ferguson@outlook.com>
2023-08-30 23:10:41 -0500

    Add project files.

---
+++ esp32-project1/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace esp32_project1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            System.Device
+
+            Thread.Sleep(Timeout.Infinite);
+
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+    }
+}
